d729913fa7fc9b93e5fb3f2a9210ca7098360a6b
Mo7ammedReyad <mo7ammedreyad@gmail.com>
2025-08-03 01:59:56 +0300

    Add files via upload

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  output: "./.open-next", // دا المكان اللي بيحط فيه ملفات البناء
+};
+
+export default config;
